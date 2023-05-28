f51ad7cc33f8f43e28172a6798f9bfe0b093e4ed
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-05-28 12:51:24 +0300

    multi-envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,6 @@ import redisConfig from './config/redis.config';
 import { FirebaseModule } from './modules/firebase/firebase.module';
 import { UserLogsModule } from './modules/userLogs/userLogs.module';
 import { TasksModule } from './modules/tasks/tasks.module';
-import { SimplifiModule } from './modules/simplifi/simplifi.module';
 import simplifiConfig from './config/simplifi.config';
 
 const serviceAccount = require('../serviceAccountKey.json');
@@ -33,8 +32,8 @@ const serviceAccount = require('../serviceAccountKey.json');
     AuthModule,
     FirebaseModule,
     UserLogsModule,
-    SimplifiModule,
     ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/env/${process.env.NODE_ENV}.env`,
       load: [
         jwtConfig,
         databaseConfig,
